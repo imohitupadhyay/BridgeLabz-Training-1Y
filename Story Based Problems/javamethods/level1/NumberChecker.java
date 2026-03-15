4fa8544a7c2b4c660b5b3f1f79949537ffdbd95a
Mohit Upadhyay <mohitv.upadhyay@gmail.com>
2026-03-15 23:48:52 +0530

    Syncing folder structure and adding Story Based Problems

--- Story Based Problems/javamethods/level1/NumberChecker.java
+++
@@ -1,7 +0,0 @@
-public class NumberChecker {
-    public static int checkNumber(int n) {
-        if (n > 0) return 1; // [cite: 93]
-        if (n < 0) return -1; // [cite: 93]
-        return 0; // [cite: 93]
-    }
-}
