c4cfae11e90d8399effde4e8934e5107e86007e7
imohitupadhyay <mohitv.upadhyay@gmail.com>
2026-03-16 10:54:11 +0530

    Delete basic-programs directory

--- basic-programs/02-Java-Primitive-Data/Level-1/PCMAverage.java
+++
@@ -1,8 +0,0 @@
-public class PCMAverage {
-    public static void main(String[] args) {
-        int maths = 94, physics = 95, chemistry = 96;
-        int totalMarks = maths + physics + chemistry;
-        double averagePercent = totalMarks / 3.0;
-        System.out.println("Sam's average mark in PCM is " + averagePercent);
-    }
-}
