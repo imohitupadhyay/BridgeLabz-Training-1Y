8a114fa8947f259b03f263176fbc762f445b0a8f
Mohit Upadhyay <mohitv.upadhyay@gmail.com>
2026-04-27 10:47:55 +0530

    Delete basic-programs/14-wrapper-classes directory

--- basic-programs/14-wrapper-classes/PrimitiveToWrapper.java
+++
@@ -1,1 +0,0 @@
-import java.util.*; class PrimitiveToWrapper{ public static void main(String[] args){ Scanner sc=new Scanner(System.in); int a=sc.nextInt(); Integer obj=a; System.out.println(a); System.out.println(obj);} }
