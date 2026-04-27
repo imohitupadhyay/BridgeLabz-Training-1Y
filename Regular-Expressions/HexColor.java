2ecc55c55d3a5c62b2d8131a1b838406621a2672
Mohit Upadhyay <mohitv.upadhyay@gmail.com>
2026-04-27 10:49:03 +0530

    Delete Regular-Expressions directory

--- Regular-Expressions/HexColor.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class HexColor{
-public static void main(String[] args){
-Scanner sc=new Scanner(System.in);
-String s=sc.next();
-if(s.matches("#[0-9a-fA-F]{6}")) System.out.println("Valid");
-else System.out.println("Invalid");
-}
-}
