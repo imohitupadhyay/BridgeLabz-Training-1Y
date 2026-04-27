bf18115b9b1844c56b061275fb3c4e4dd58fab5d
Mohit Upadhyay <mohitv.upadhyay@gmail.com>
2026-04-27 10:47:31 +0530

    Delete Regular-Expressions directory

--- Regular-Expressions/CreditCard.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class CreditCard{
-public static void main(String[] args){
-Scanner sc=new Scanner(System.in);
-String s=sc.next();
-if(s.matches("[45]\\d{15}")) System.out.println("Valid");
-else System.out.println("Invalid");
-}
-}
--- Regular-Expressions/UsernameValidation.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class UsernameValidation{
-public static void main(String[] args){
-Scanner sc=new Scanner(System.in);
-String s=sc.next();
-if(s.matches("[a-zA-Z][a-zA-Z0-9_]{4,14}")) System.out.println("Valid");
-else System.out.println("Invalid");
-}
-}
